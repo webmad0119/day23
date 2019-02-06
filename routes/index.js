c2902a1c4b7ad32f5c9a0d39b4e579d1c3b53407
Dani Vicario <danigv.email@gmail.com>
2019-02-06 13:18:44 +0100

    added users removal

--- routes/index.js
+++ routes/index.js
@@ -30,16 +30,30 @@ router.put('/posts/:id', (req, res, next) => {
 });
 
 router.get('/users/:limit', (req, res, next) => {
+  User.find({}).limit(+req.params.limit).then(users => {
+    res.json({
+      users
+    })
+  })
+});
 
+router.get('/add-user', (req, res, next) => {
+  res.render("add-user-view")
 });
 
 router.get('/users/:id', (req, res, next) => {
 
 });
 
+router.get('/list-users', (req, res, next) => {
+  User.find({}).then(users => {
+    res.render("list-users", { users })
+  })
+});
+
 router.post('/users', (req, res, next) => {
   User.create(req.body).then(sucessPayload => {
-    res.json({ ok: true, success: sucessPayload})
+    res.json({ ok: true, success: sucessPayload })
   })
     .catch(errorPayload => {
       res.json({ ok: false, error: errorPayload })
@@ -47,7 +61,11 @@ router.post('/users', (req, res, next) => {
 });
 
 router.delete('/users/:id', (req, res, next) => {
-
+  User.findByIdAndDelete(req.params.id).then(deletedPayload => {
+    User.find({}).then(usersPayload => {
+      res.json({users: usersPayload})
+    })
+  })
 });
 
 router.put('/users/:id', (req, res, next) => {
