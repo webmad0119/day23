8266a64dd1601ac714e107db533ecc5991dbf556
Dani Vicario <danigv.email@gmail.com>
2019-02-06 11:45:44 +0100

    added routes for the restful endpoints

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,45 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('index');
+});
+
+router.get('/posts/:limit', (req, res, next) => {
+
+});
+
+router.get('/posts/:id', (req, res, next) => {
+
+});
+
+router.delete('/posts/:id', (req, res, next) => {
+
+});
+
+router.post('/posts', (req, res, next) => {
+
+});
+
+router.put('/posts/:id', (req, res, next) => {
+
+});
+
+router.get('/users/:limit', (req, res, next) => {
+
+});
+
+router.get('/users/:id', (req, res, next) => {
+
+});
+
+router.delete('/users/:id', (req, res, next) => {
+
+});
+
+router.put('/users/:id', (req, res, next) => {
+
 });
 
 module.exports = router;
